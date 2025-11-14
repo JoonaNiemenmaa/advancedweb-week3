1ab018f7663221a40f1f4c1028aa81b1f28f0e8a
Joona Niemenmaa <joonaoskari@hotmail.com>
2025-11-14 20:13:19 +0200

    korjattiin html idt

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 const user_form = document.getElementById("userForm");
 const name_input = document.getElementById("name");
 const email_input = document.getElementById("email");
-const get_users_button = document.getElementById("getUsersButton");
+const get_users_button = document.getElementById("getUsers");
 
 user_form.addEventListener("submit", (event) => {
 	event.preventDefault();
@@ -26,7 +26,7 @@ user_form.addEventListener("submit", (event) => {
 });
 
 function display_users(users) {
-	const user_list = document.getElementById("user_list");
+	const user_list = document.getElementById("userList");
 	user_list.innerHTML = "";
 	for (const user of users) {
 		const list_item = document.createElement("li");
