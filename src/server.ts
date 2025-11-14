176a989f8710dca8be63f9e203110ed1d846f691
Joona Niemenmaa <joonaoskari@hotmail.com>
2025-11-14 12:12:38 +0200

    Palvelin nyt kuuntelee myös '/' reittiä

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+	res.send("It's working!");
+});
+
 app.get("/hello", (req, res) => {
 	const response_json = {
 		msg: "Hello world!",
