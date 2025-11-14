1454c4b9d7bc41f38d6506d88347df339a940a63
Joona Niemenmaa <joonaoskari@hotmail.com>
2025-11-14 13:11:19 +0200

    task 3 done

--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,34 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("It's working!");
+app.use(express.json());
+
+app.get("/", (request, response) => {
+	response.send("It's working!");
 });
 
-app.get("/hello", (req, res) => {
+app.get("/hello", (request, response) => {
 	const response_json = {
 		msg: "Hello world!",
 	};
-	res.send(response_json);
+	response.send(response_json);
 });
 
 app.get("/echo/:id", (request, response) => {
 	response.send(request.params);
+});
+
+app.post("/sum", (request, response) => {
+	console.log(request.body);
+	let numbers: Array<number> = request.body.numbers;
+	let sum = 0;
+	for (let number of numbers) {
+		sum += number;
+	}
+	const response_json = {
+		sum: sum,
+	};
+	response.send(response_json);
 });
 
 app.listen(port, () => {
